430ec097b2cac94b95fe25bdd4579ddf4d8ed1b9
Ron <ande1218@gmail.com>
2020-04-09 00:17:18 +0800

    refactor: remove useless code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from 'redux'
-import todos from './todos'
-import visibilityFilter from './visibilityFilter'
 import auth from './auth'
 import loading from './loading'
 
 export default combineReducers({
-  todos,
-  visibilityFilter,
   auth,
   loading
 })
