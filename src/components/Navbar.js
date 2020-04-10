57d265ddf8a38d972a0ee8024f4705b080cca2c5
Ron.Ho <Ron.Ho@advantech.com>
2020-04-10 15:58:25 +0800

    feat: add LoginPage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { connect } from 'react-redux'
-import PropTypes from "prop-types";
-import clsx from 'clsx';
 import { Link } from 'react-router-dom';
 
 import { makeStyles } from "@material-ui/core/styles";
--- src/views/LoginPage.js
+++ src/views/LoginPage.js
@@ -1,15 +1,91 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { doLogin } from '../actions/auth'
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  }
+}));
 
 const LoginPage = ({ state, dispatch }) => {
+
+  const classes = useStyles();
+
+  const [email, setEmail] = React.useState('')
+
+  const [password, setPassword] = React.useState('')
+
   return (
-    <form>
-      <button onClick={ e =>{
-        e.preventDefault()
-        dispatch(doLogin({email: 'root', passWord: '123456'}))
-      }}>Login</button>
-    </form>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={ e => {
+          e.preventDefault()
+          dispatch(doLogin({ email: email, passWord: password }))
+        }}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            onChange={(e) => setEmail(e.target.value)}
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+        </form>
+      </div>
+    </Container>
   )
 }
 
